631e9476966273001a114a0da6fc0fc1a7b7c9e2
James <uptownhr@gmail.com>
2024-03-18 11:54:32 -0700

    chore: adjusted main page styles

--- faq/app/page.tsx
+++ faq/app/page.tsx
@@ -1,7 +1,9 @@
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <a href="/new">Start a new topic</a>
+    <main className="flex min-h-screen flex-col items-center justify-between p-1">
+      <a className="p-4 w-full border-2" href="/new">
+        Start a New Topic
+      </a>
     </main>
   );
 }

5ca4e554c86b92ce6425c28df73a6d948dd36594
James <uptownhr@gmail.com>
2024-03-18 11:57:37 -0700

    chore: adjusted topic to faq

--- faq/app/page.tsx
+++ faq/app/page.tsx
@@ -2,7 +2,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-1">
       <a className="p-4 w-full border-2" href="/new">
-        Start a New Topic
+        Start a New FAQ
       </a>
     </main>
   );

51ae0292995552e4a0b281f9b682d1a9265046b4
James <uptownhr@gmail.com>
2024-03-17 21:19:42 -0700

    changed home page to link to new topic

--- faq/app/page.tsx
+++ faq/app/page.tsx
@@ -1,17 +1,7 @@
-import { List } from "@/app/List";
-import { getQuestionAction } from "@/app/get-questions.action";
-import { createQuestionAction } from "@/app/create-question.action";
-
 export default async function Home() {
-  const questions = await getQuestionAction({ title: "test 1234" });
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form action={createQuestionAction}>
-        <input type="text" name="title" />
-        <button type="submit">Update User Name</button>
-      </form>
-      <List x={questions} />
+      <a href="/new">Start a new topic</a>
     </main>
   );
 }
